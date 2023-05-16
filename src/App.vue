83f9dbab0270e63ed86b19ffb79d7b4ea8fcd3ec
Lermy Ferrer <lermy.ferrer@insoftar.com>
2023-05-16 14:01:01 -0500

    init mode dark

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,33 @@
 <script setup>
+import { ref } from "vue";
+import { useThemeDark } from './composables/useThemeDark'
+
 import HomeView from "./views/HomeView.vue";
+
+//const { modeDarkLocalstorage } = useThemeDark();
+
+if (
+      localStorage.theme === "dark" ||
+      (!("theme" in localStorage) &&
+        window.matchMedia("(prefers-color-scheme: dark)").matches)
+    ) {
+      document.documentElement.classList.add("dark");
+    } else {
+      document.documentElement.classList.remove("dark");
+    }
+
+const toggleDarkMode = ref(document.documentElement.className ==='dark');
+
+const changeDarkMode = () => {
+  toggleDarkMode.value = document.documentElement.classList.add('dark');
+  toggleDarkMode.value ? localStorage.theme = 'dark' : localStorage.theme = 'light';
+}
 </script>
 
 <template>
+    <!-- <div class="flex justify-end p-4">
+    <button @click="changeDarkMode">dia/noche</button>
+  </div> -->
   <HomeView
     message="Lermy is systems engineer graduated from the Corporación Universitaria Latinoamericana (CUL) in Barranquilla, Colombia. Web developer with over 3 years of experience." description="Developing applications with: PHP | Laravel | Javascript | Vue.js | Angular | Typescript | Tailwind"
   />
---
+++ src/composables/useThemeDark.ts
@@ -0,0 +1,15 @@
+export function useThemeDark() {
+  const modeDarkLocalstorage = () => {
+    if (
+      localStorage.theme === "dark" ||
+      (!("theme" in localStorage) &&
+        window.matchMedia("(prefers-color-scheme: dark)").matches)
+    ) {
+      document.documentElement.classList.add("dark");
+    } else {
+      document.documentElement.classList.remove("dark");
+    }
+  };
+
+  return { modeDarkLocalstorage };
+}
